c4bc544d98d903b0dd988ea4cb5426eb4fee7afd
mammad8113 <98411394+mammad8113@users.noreply.github.com>
2025-12-12 16:10:50 +0330

    add api item

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from app.model.itemModle import Item
 app = FastAPI()
 
 @app.get("/")
@@ -10,3 +10,11 @@ def root():
 @app.get("/test")
 def root():
     return {"message":"test"}
+
+
+@app.post("/items")
+def create_item(item: Item):
+    return {
+        "status": "created",
+        "item": item
+    }
